502dd6300f0192faf5888c0bb7cc7747cdf60ca6
Harsh Mehta <mharsh13dec@gmail.com>
2021-05-30 18:52:11 +0530

    base setup!

---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  // void initState() {
+  //   super.initState();
+  //   getcurrentUser();
+  // }
+
+  // Future<void> getcurrentUser() async {
+  //   final user = FirebaseAuth.instance.currentUser;
+  //   if (user == null) {
+  //     Future.delayed(Duration(seconds: 3)).then((value) {
+  //       Navigator.of(context).pushReplacement(
+  //         MaterialPageRoute(
+  //           builder: (BuildContext context) => LogInScreen(),
+  //         ),
+  //       );
+  //     });
+  //     return;
+  //   }
+  //   if (user != null) {
+  //     Future.delayed(Duration(seconds: 3)).then((value) {
+  //       Navigator.of(context).pushReplacement(
+  //         MaterialPageRoute(
+  //           builder: (BuildContext context) => HomeScreen(),
+  //         ),
+  //       );
+  //     });
+  //     return;
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor("#F3DDE6"),
+      body: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: 0.2,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                "Assets/background.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Image.asset(
+                "Assets/splashLogo.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
